aeedbc1deebb48d052fd625ad76eb3fcf3c15c43
Jason Tan <tanjunxiang@igengmei.com>
2020-12-22 09:58:42 +0800

    fix(register):调整行距

--- api/register.php
+++ api/register.php
@@ -3,8 +3,8 @@ include('config.php');
 $jsonContent = json_decode(file_get_contents("php://input"), true);
 $nickname = $jsonContent['username'];
 $password = $jsonContent['password'];
-
 $qyjBackendSql = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
 if ($qyjBackendSql) {
     die('Connect Error:' . mysqli_error($qyjBackendSql));
 } else {
