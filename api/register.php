6de4960c31205c4c916fa422cfa18f2d2a24c086
Adian-kids <adian@wgpsec.org>
2020-12-22 13:07:46 +0800

    fix:修复数据库连接问题

--- api/register.php
+++ api/register.php
@@ -4,22 +4,19 @@ $jsonContent = json_decode(file_get_contents("php://input"), true);
 $nickname = $jsonContent['username'];
 $password = $jsonContent['password'];
 $qyjBackendSql = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
-if ($qyjBackendSql) {
+if (!$qyjBackendSql) {
     die('Connect Error:' . mysqli_error($qyjBackendSql));
-} else {
-    echo "Mysql Connect Successfully";
 }
-
 $sqlUserInsert = "INSERT INTO user " .
     "(nicknme,password) " .
     "VALUES " .
     "('$nickname','$password')";
-    
 $sqlReturnValue = mysqli_query($qyjBackendSql, $sqlUserInsert);
-if (!sqlReturnValue) {
+if (!$sqlReturnValue) {
     die('Insert Error: ' . mysqli_error($qyjBackendSql));
+}else{
+	$registerResult = "Success";
 }
-echo "Insert Successfully\n";
 mysqli_close($qyjBackendSql);
+echo $registerResult;
 ?>
