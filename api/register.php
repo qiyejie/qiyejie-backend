082a5d22bfca584896aa58c4aa3800f8e2266aec
Adian-kids <adian@wgpsec.org>
2020-12-22 19:15:15 +0800

    fix(register):修复注册失败，重复注册，明文存放数据库的问题

--- api/register.php
+++ api/register.php
@@ -1,21 +1,33 @@
 <?php
-include('config.php');
+include('config.php'); //配置数据库信息
+$registerResult = "None"; //初始化返回状态
 $jsonContent = json_decode(file_get_contents("php://input"), true);
 $nickname = $jsonContent['username'];
-$password = $jsonContent['password'];
+$password = md5($jsonContent['password']);
 $qyjBackendSql = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if (!$qyjBackendSql) {
     die('Connect Error:' . mysqli_error($qyjBackendSql));
 }
+
+$sqlCheckRegist = "select * from user where nickname='$nickname'";
+
 $sqlUserInsert = "INSERT INTO user " .
-    "(nicknme,password) " .
+    "(nickname,password) " .
     "VALUES " .
     "('$nickname','$password')";
-$sqlReturnValue = mysqli_query($qyjBackendSql, $sqlUserInsert);
-if (!$sqlReturnValue) {
-    die('Insert Error: ' . mysqli_error($qyjBackendSql));
-}else{
-	$registerResult = "Success";
+if ($nickname) {
+    $checkRegistResult = mysqli_query($qyjBackendSql,$sqlCheckRegist);
+    if(mysqli_num_rows($checkRegistResult) > 0){
+        die('此用户名已被注册');
+    }else{
+        $sqlReturnValue = mysqli_query($qyjBackendSql, $sqlUserInsert);
+        if (!$sqlReturnValue) {
+            die('Insert Error: ' . mysqli_error($qyjBackendSql));
+        }else{
+	    $registerResult = "注册成功";
+        }
+    }
+
 }
 mysqli_close($qyjBackendSql);
 echo $registerResult;
