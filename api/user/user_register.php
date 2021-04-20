de07d9eef5e18a1da1f66bd723f99e53f0b866b3
Jason Tan <tanjunxiang@guazi.com>
2021-04-21 00:39:43 +0800

    fix

--- api/user/user_register.php
+++ api/user/user_register.php
@@ -9,7 +9,7 @@ $user_nickname = $user_info['nickname'];
 $user_pass = $user_info['password'];
 $qyj_id = mt_rand(1,1000)*date("i")*date("s");
 // 构造数据库插入语句
-$insert_sql = "INSERT INTO users (qyj_id,nickname,password) VALUES ('".$user_nickname."','".$user_pass."','".$qyj_id."')";
+$insert_sql = "INSERT INTO users (qyj_id,nickname,password) VALUES ('".$qyj_id."','".$user_nickname."','".$user_pass."')";
 // 创建连接
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 // 检测连接
