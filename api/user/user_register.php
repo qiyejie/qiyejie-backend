0bc93321c0c3c0c5984f8ee04e13075f39dc6ad4
Jason Tan <tanjunxiang@guazi.com>
2021-04-21 00:16:55 +0800

    fix

--- api/user/user_register.php
+++ api/user/user_register.php
@@ -8,18 +8,20 @@ $user_info = json_decode(file_get_contents("php://input"), true);
 $user_nickname = $user_info['nickname'];
 $user_pass = $user_info['password'];
 // 构造数据库插入语句
-$insert_sql = "INSERT INTO users (nickname,password) VALUES ('$user_nickname','$user_pass')";
+$insert_sql = "INSERT INTO `users` (`nickname`,`password`) VALUES ($user_nickname,$user_pass)";
 // 连接数据库
 $mysql_connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 // 测试数据库连接状态
 if (!$mysql_connect) {
-    die('mysql_connect error:'.mysqli_error($mysql_connect));
+  die('mysql_connect error:'.mysqli_error($mysql_connect));
 }
 // 数据库进行插入操作
 $insert_sql_result = mysqli_query($mysql_connect,$insert_sql);
 // 返回插入结果
 if ($insert_sql_result){
-    $register_result = 0;
+  $register_result = 0;
+} else {
+  die('系统异常~');
 }
 // 获取qyj_id
 $qyj_id = mysqli_insert_id($mysql_connect);
