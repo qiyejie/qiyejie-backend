5912b0c7c0195a0c697521ab80dbc4dbda80e040
Jason Tan <tanjunxiang@guazi.com>
2021-04-21 01:24:11 +0800

    fix:修改状态码和返回参数

--- api/user/user_register.php
+++ api/user/user_register.php
@@ -2,14 +2,15 @@
 // 载入数据库配置
 include('../../config.php');
 // 初始化返回信息
-$register_result = -1;
+$code = -1;
 // 处理传入的信息
 $user_info = json_decode(file_get_contents("php://input"), true);
 $user_nickname = $user_info['nickname'];
 $user_pass = $user_info['password'];
-$qyj_id = mt_rand(1,1000)*date("i")*date("s");
+$qyj_id = time()*date("s");
+$sign_in_time = time();
 // 构造数据库插入语句
-$insert_sql = "INSERT INTO users (qyj_id,nickname,password) VALUES ('$qyj_id','$user_nickname','$user_pass')";
+$insert_sql = "INSERT INTO users (qyj_id,nickname,password,sign_in_time) VALUES ('$qyj_id','$user_nickname','$user_pass','$sign_in_time')";
 // 创建连接
 $mysql_connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 // 检测连接
@@ -19,13 +20,13 @@ if (mysqli_connect_errno($mysql_connect)) {
 // 数据库进行插入操作
 $insert_sql_result = mysqli_query($mysql_connect,$insert_sql);
 // 返回插入结果
-if ($insert_sql_result){
-  $register_result = 0;
+if ($insert_sql_result) {
+  $code = 0;
 } else {
   die('系统异常~');
 }
 // 处理返回数组
-$return_array = array("code"=>$register_result,"qyj_id"=>$qyj_id,"message"=>"注册成功");
+$return_array = array("code"=>$code,"qyj_id"=>$qyj_id,"message"=>"注册成功");
 // 关闭数据库连接
 mysqli_close($mysql_connect);
 // 返回注册结果
