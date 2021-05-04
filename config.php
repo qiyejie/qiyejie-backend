92ff7d9cc85e2b595978f46933f8a67dc87496d4
Jason Tan <tanjunxiang@guazi.com>
2021-05-05 00:41:01 +0800

    fix

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin:*');
-header('Access-Control-Allow-Methods:*');
+header("Content-type: application/json; charset=utf-8");
 $db_host = "localhost";
 $db_user = "backend";
 $db_pass = "qiyejie";
