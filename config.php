d7fca67e164d973665190307888e312867b5fa1c
Adian-kids <admin@e-wolf.top>
2021-05-07 17:03:10 +0800

    feat:增加cookie验证功能(暂时注释)

--- config.php
+++ config.php
@@ -4,7 +4,25 @@ $db_host = "localhost";
 $db_user = "backend";
 $db_pass = "qiyejie";
 $db_name = "backend";
-echo "config";
 $mysql_connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+// 判断数据库连接状态
 
+if(!$mysql_connect){
+    die("Mysql Connect Error".mysqli_error($mysql_connect));
+}
+/*
+// 接收cookies
+$qyj_id = $_POST['qyj_id'];
+$user_cookies = $_COOKIE['qyj_cookies'];
+// 构造SQL查询语句
+$cookies_check_sql = "SELECT cookies FROM users WHERE qyj_id=$qyj_id";
+// 进入数据库执行
+$cookies_check_sql_result = mysqli_query($mysql_connect,$cookies_check_sql);
+// 读取数据
+$cookies_result = mysqli_fetch_assoc($cookies_check_sql_result);
+$cookies = $cookies_result['cookies'];
+if($cookies != $user_cookies){
+    die("Cookies无效");    
+}
+*/
 ?>
